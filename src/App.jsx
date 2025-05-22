40f6e1e554231575aae004cfc3b93427a963d154
Jyfa25 <jyfabot@gmail.com>
2025-05-22 13:26:31 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Jyfa SurveyBot</h1>
+      <p>This is a working Vercel-ready React setup using Vite.</p>
+    </div>
+  )
+}
+
+export default App
